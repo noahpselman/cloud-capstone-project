b435489c019ff42480c1ef52a10ea808096e3a23
Ubuntu <ubuntu@ip-172-31-24-163.ec2.internal>
2021-05-29 17:43:20 -0500

    cosmetic changes to archive

--- util/archive/archive_app.py
+++ util/archive/archive_app.py
@@ -59,10 +59,9 @@ def archive_free_user_data():
 	except ClientError:
 		print("Problem receiving messages")
 	try:
-		[message] = response['Messages']
-		receipt_handle = message['ReceiptHandle']
-		body = json.loads(message['Body'])
-		content = json.loads(body['Message'])
+		messages = response['Messages']
+		print(f"found {len(messages)} messages")
+		# [message] = response['Messages']
 	except KeyError as e:
 		print(f"no messages found:", e)
 		return abort(500)
@@ -74,71 +73,82 @@ def archive_free_user_data():
 
 	# extract data
 
-	job_id = content['job_id']
-	user_id = content['user_id']
-	s3_key_result_file = content['s3_key_result_file']
+	for message in messages:
+		receipt_handle = message['ReceiptHandle']
+		body = json.loads(message['Body'])
+		content = json.loads(body['Message'])
+
+		job_id = content['job_id']
+		user_id = content['user_id']
+		s3_key_result_file = content['s3_key_result_file']
 
 
-	user_role = helpers.get_user_profile(user_id)['role']
-	print("user role is", user_role)
-
-	if user_role == 'free_user':
-
-		# get s3
+		user_role = helpers.get_user_profile(user_id)['role']
+		print("user role is", user_role)
 		try:
 			s3_client = boto3.client('s3', region_name=app.config['AWS_REGION_NAME'])
-			obj = s3_client.get_object(Bucket=app.config['AWS_S3_RESULTS_BUCKET'], 
-									Key=s3_key_result_file)
-			content = obj['Body'].read()
 		except ClientError as e:
+			print("problem creating s3 client:", e)
 			return abort(500)
 
-		# archive to dynamo
+		if user_role == 'free_user':
+
+			# get s3
+			try:
+				print("trying s3 key:::::", s3_key_result_file)
+				obj = s3_client.get_object(Bucket=app.config['AWS_S3_RESULTS_BUCKET'], 
+										Key=s3_key_result_file)
+				content = obj['Body'].read()
+			except ClientError as e:
+				print("failed get object from s3:", e)
+				return abort(500)
+
+			# archive to dynamo
+			try:
+				glacier_client = boto3.client('glacier', region_name=app.config['AWS_REGION_NAME'])
+				response = glacier_client.upload_archive(vaultName=app.config['AWS_VAULT_NAME'],
+											body=content)
+				archive_id = response['archiveId']
+				# print("glacier response", response)
+			except ClientError as e:
+				print("Problem uploading to glacier", e)
+				return abort(500)
+
+			# upload glacier id to dynamo
+			print("updating dynamo with ", job_id)
+			update_expression = "SET archive_status = :new_archive_status, archive_id = :new_archive_id"
+			expression_vals = {":new_archive_id": {"S": archive_id}, 
+							":not_archived": {"S": "NOT_ARCHIVED"},
+							":new_archive_status": {'S': 'ARCHIVED'}}
+			condtion_expression = "archive_status = :not_archived"
+			try:
+				dynamo_client = boto3.client('dynamodb', region_name=app.config['AWS_REGION_NAME'])
+				response = dynamo_client.update_item(TableName=app.config['AWS_DYNAMODB_ANNOTATIONS_TABLE'], 
+														Key={'job_id': {'S': job_id}}, 
+														UpdateExpression=update_expression,
+														ExpressionAttributeValues=expression_vals,
+														ConditionExpression=condtion_expression)
+			except ClientError as e:
+				print("There was an issue uploading the results to the Dynamo Table:", e)
+				return abort(500)
+
+
+			try:
+				response = s3_client.delete_object(
+					Bucket=app.config['AWS_S3_RESULTS_BUCKET'],
+					Key=s3_key_result_file
+				)
+			except ClientError as e:
+				print("problem deleting s3 object", e)
+				return abort(500)
+
+		# delete message
 		try:
-			glacier_client = boto3.client('glacier', region_name=app.config['AWS_REGION_NAME'])
-			response = glacier_client.upload_archive(vaultName=app.config['AWS_VAULT_NAME'],
-										body=content)
-			archive_id = response['archiveId']
-			print("glacier response", response)
+			sqs_client.delete_message(QueueUrl=app.config['AWS_ARCHIVE_MESSAGE_QUEUE'], 
+									ReceiptHandle=receipt_handle)
 		except ClientError as e:
-			print("Problem uploading to glacier", e)
+			print("There was an error deleting message the message from the SQS:", e)
 			return abort(500)
-
-		# upload glacier id to dynamo
-		print("updating dynamo with ", job_id)
-		update_expression = "SET archive_status = :new_archive_status, archive_id = :new_archive_id"
-		expression_vals = {":new_archive_id": {"S": archive_id}, 
-						":not_archived": {"S": "NOT_ARCHIVED"},
-						":new_archive_status": {'S': 'ARCHIVED'}}
-		condtion_expression = "archive_status = :not_archived"
-		try:
-			dynamo_client = boto3.client('dynamodb', region_name=app.config['AWS_REGION_NAME'])
-			response = dynamo_client.update_item(TableName=app.config['AWS_DYNAMODB_ANNOTATIONS_TABLE'], 
-													Key={'job_id': {'S': job_id}}, 
-													UpdateExpression=update_expression,
-													ExpressionAttributeValues=expression_vals,
-													ConditionExpression=condtion_expression)
-		except ClientError as e:
-			print("There was an issue uploading the results to the Dynamo Table:", e)
-			return abort(500)
-
-	# delete s3 object
-	try:
-		response = s3_client.delete_object(
-			Bucket=app.config['AWS_S3_RESULTS_BUCKET'],
-			Key=s3_key_result_file
-		)
-	except ClientError as e:
-		print("problem deleting s3 object", e)
-		return abort(500)
-
-	# delete message
-	try:
-		sqs_client.delete_message(QueueUrl=app.config['AWS_ARCHIVE_MESSAGE_QUEUE'], 
-								  ReceiptHandle=receipt_handle)
-	except ClientError as e:
-		print("There was an error deleting message the message from the SQS:", e)
-		return abort(500)
 
 	return {"Response": "Message archived"}, 201
 
