c45b38dfedc5f3d044d8a084dfb3b43aed825715
Ubuntu <ubuntu@ip-172-31-24-163.ec2.internal>
2021-05-29 19:48:06 -0500

    made notify.py prettier

--- util/archive/archive_app.py
+++ util/archive/archive_app.py
@@ -31,15 +31,16 @@ def home():
 
 @app.route('/archive', methods=['POST'])
 def archive_free_user_data():
+
 	# read notification
 	try:
 		data = json.loads(request.data)
-		message_id = data['MessageId']
 		message = json.loads(data['Message'])
 		topic_arn = data["TopicArn"]
 	except KeyError as e:
 		print("invalid inputs:", e)
 		abort(500)
+		
 
 	if topic_arn != app.config['AWS_ARCHIVE_SNS']:
 		abort(405)
@@ -56,35 +57,34 @@ def archive_free_user_data():
 		QueueUrl=app.config['AWS_ARCHIVE_MESSAGE_QUEUE'], 
 		MaxNumberOfMessages=10, 
 		WaitTimeSeconds=20)
-	except ClientError:
-		print("Problem receiving messages")
+	except ClientError as e:
+		print("Problem receiving messages:", e)
+		return abort(500)
 	try:
 		messages = response['Messages']
 		print(f"found {len(messages)} messages")
-		# [message] = response['Messages']
 	except KeyError as e:
-		print(f"no messages found:", e)
+		print(f"No messages found:", e)
 		return abort(500)
-
-	# sqs_message_id = body['MessageId']
-	# if message_id != sqs_message_id:
-	# 	print("message doesn't match")
-	# 	return abort(500)
 
 	# extract data
 
 	for message in messages:
-		receipt_handle = message['ReceiptHandle']
-		body = json.loads(message['Body'])
-		content = json.loads(body['Message'])
+		try:
+			receipt_handle = message['ReceiptHandle']
+			body = json.loads(message['Body'])
+			content = json.loads(body['Message'])
 
-		job_id = content['job_id']
-		user_id = content['user_id']
-		s3_key_result_file = content['s3_key_result_file']
+			job_id = content['job_id']
+			user_id = content['user_id']
+			s3_key_result_file = content['s3_key_result_file']
+		except KeyError as e:
+			print("Message doesn't have correct fields:", e)
+			abort(500)
 
 
 		user_role = helpers.get_user_profile(user_id)['role']
-		print("user role is", user_role)
+
 		try:
 			s3_client = boto3.client('s3', region_name=app.config['AWS_REGION_NAME'])
 		except ClientError as e:
@@ -92,30 +92,31 @@ def archive_free_user_data():
 			return abort(500)
 
 		if user_role == 'free_user':
+			print("free user... archiving results")
 
 			# get s3
 			try:
-				print("trying s3 key:::::", s3_key_result_file)
+				print("getting result objects from s3...")
 				obj = s3_client.get_object(Bucket=app.config['AWS_S3_RESULTS_BUCKET'], 
 										Key=s3_key_result_file)
 				content = obj['Body'].read()
 			except ClientError as e:
-				print("failed get object from s3:", e)
+				print("Failed get object from s3:", e)
 				return abort(500)
 
-			# archive to dynamo
+			# archiving to glacier
 			try:
+				print("uploading to glacier...")
 				glacier_client = boto3.client('glacier', region_name=app.config['AWS_REGION_NAME'])
 				response = glacier_client.upload_archive(vaultName=app.config['AWS_VAULT_NAME'],
 											body=content)
 				archive_id = response['archiveId']
-				# print("glacier response", response)
 			except ClientError as e:
-				print("Problem uploading to glacier", e)
+				print("Problem uploading to glacier:", e)
 				return abort(500)
 
 			# upload glacier id to dynamo
-			print("updating dynamo with ", job_id)
+			print(f"updating dynamo table with job_id {job_id} to store archive id...")
 			update_expression = "SET archive_status = :new_archive_status, archive_id = :new_archive_id"
 			expression_vals = {":new_archive_id": {"S": archive_id}, 
 							":not_archived": {"S": "NOT_ARCHIVED"},
@@ -132,26 +133,35 @@ def archive_free_user_data():
 				print("There was an issue uploading the results to the Dynamo Table:", e)
 				return abort(500)
 
-
 			try:
+				print("deleting results file from s3...")
 				response = s3_client.delete_object(
 					Bucket=app.config['AWS_S3_RESULTS_BUCKET'],
 					Key=s3_key_result_file
 				)
 			except ClientError as e:
-				print("problem deleting s3 object", e)
+				print("Problem deleting s3 object", e)
 				return abort(500)
 
 		# delete message
 		try:
+			print("deleting message from queue...")
 			sqs_client.delete_message(QueueUrl=app.config['AWS_ARCHIVE_MESSAGE_QUEUE'], 
 									ReceiptHandle=receipt_handle)
 		except ClientError as e:
 			print("There was an error deleting message the message from the SQS:", e)
 			return abort(500)
 
-	return {"Response": "Message archived"}, 201
+	return "Success", 201
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('error.html', 
+		title='Page not found', alert_level='warning',
+		message="The page you tried to reach does not exist. \
+			Please check the URL and try again."
+		), 404
 
 @app.errorhandler(405)
 def not_allowed(e):
