78657bba0392d70a0ee6fbce35123419d3950dad
Noah <noahpselman@gmail.com>
2021-06-01 20:58:44 -0500

    added file for testing annotator scaling

--- util/ann_load.py
+++ util/ann_load.py
@@ -16,26 +16,30 @@ import boto3
 from botocore.exceptions import ClientError
 
 # Define constants here; no config file is used for this scipt
-USER_ID = "<UUID_for_your_Globus_Auth_identity>"
-EMAIL = "<CNetID>@uchicago.edu"
+USER_ID = "794afb63-0059-466f-9818-6fa5a1409871"
+TOPIC = "arn:aws:sns:us-east-1:127134666975:nselman_job_requests"
+
 
 """Fires off annotation jobs with hardcoded data for testing
 """
+
+
 def load_requests_queue():
 
-  # Define and persist job data
+    sns_client = boto3.client("sns", region_name='us-east-1')
+    job_id = str(uuid.uuid1())
+    message = {'user_id': USER_ID, 'job_id': job_id}
+    response = sns_client.publish(TopicArn=TOPIC, Message=json.dumps(message))
+    print(response)
 
-  # Send message to request queue
-
-  pass
 
 if __name__ == '__main__':
-  while True:
-    try:
-      load_requests_queue()
-      time.sleep(3)
-    except ClientError as e:
-      print("Irrecoverable error. Exiting.")
-      sys.exit()
+    while True:
+        try:
+            load_requests_queue()
+            time.sleep(3)
+        except ClientError as e:
+            print("Irrecoverable error. Exiting.")
+            sys.exit()
 
-### EOF
+# EOF
